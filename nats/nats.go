b08a7e6d8f7b4ecf50180f64ecaf9946e2a9dc3f
Nikolay Popov <helioproduct.sync@gmail.com>
2024-05-27 09:59:00 +0300

    nats.go

---
+++ nats/nats.go
@@ -0,0 +1,23 @@
+package nats
+
+import (
+	"L0/models"
+	"encoding/json"
+	"fmt"
+
+	"github.com/nats-io/stan.go"
+)
+
+func MakeOrderHandler(orderModel models.OrderModel) func(*stan.Msg) {
+	return func(m *stan.Msg) {
+		var order models.Order
+		if err := json.Unmarshal(m.Data, &order); err != nil {
+			fmt.Printf("info: nats order handler can't unmarshal: %v\n", err)
+			return
+		}
+		if err := orderModel.Insert(order); err != nil {
+			fmt.Printf("info: can't insert order: %v | %v\n", order.Uid, err)
+			return
+		}
+	}
+}
